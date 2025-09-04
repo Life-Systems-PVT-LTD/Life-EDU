2bfbed08d08776f76ea41d1eedd96d99a3708cd0
Pasindu Nethsara <lifesystems.inc.adm.01@gmail.com>
2025-09-05 01:43:31 +0530

    create a project basic lay out and no page found file by Life Systems 2025-09-05 1.43 Am

---
+++ life-edu/src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Header() {
+  return (
+
+    <nav className='mb-6'>
+
+      <div>
+
+        <div>
+
+          <div></div>
+          <div>
+
+            <div>
+
+              <span>Courses</span>
+              <span>About</span>
+              <span>Contact</span>
+
+            </div>
+
+          </div>
+          <div></div>
+
+        </div>
+
+      </div>
+
+    </nav>
+
+  )
+
+}
+
+export default Header
