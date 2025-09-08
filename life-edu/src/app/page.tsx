81b528736a24de276381d03c2a862cb6dbf20104
Pasindu Nethsara <lifesystems.inc.adm.01@gmail.com>
2025-09-08 23:44:15 +0530

    50% completed Pasindu Nethsara 2025-09-08 11.44 Pm

--- life-edu/src/app/page.tsx
+++ life-edu/src/app/page.tsx
@@ -20,13 +20,33 @@ export default function Home() {
 
         {/* hero section */}
 
-        <div className="bg-linear-to-r items-center from-[#1E53DB] to-[#56E29C] flex flex-row p-10 justify-between h-[70vh]">
+        <div className="bg-linear-to-r items-center from-[#1E53DB] to-[#56E29C] flex flex-row p-10 justify-between h-[65vh]">
 
           {/* description section */}
 
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-6">
 
-            <span>description here</span>
+            <div className="max-w-xs text-justify leading-11">
+
+              <span className="text-[40px] font-bold text-[#020817]">Learn Skills That Matter for Your Future</span>
+              
+            </div>
+
+            <div className="max-w-xl leading-8 mt-3 tracking-wider">
+
+              <p className="text-[18px] font-semibold text-[#FFFFFF]">Master in-demand skills with our comprehensive courses taught by industry experts. Start your learning journey today and transform your career</p>
+
+            </div>
+
+            {/* button */}
+            
+            <div className="mt-7">
+
+              <button className="bg-[#0F172A] p-3 text-center cursor-pointer font-semibold rounded-sm text-[#FFFFFF]">Browse Courses</button>            
+
+            </div>
+
+            {/* button */}
 
           </div>
 
@@ -36,7 +56,7 @@ export default function Home() {
 
           <div className="flex flex-col">
 
-            <Image src={'/assets/images/herosectiongroupi1.svg'} width={600} height={600} alt="hero section group image" />
+            <Image src={'/assets/images/herosectiongroupi1.svg'} width={600} height={600} alt="hero section group image" className="rounded-sm" />
 
           </div>
 
@@ -46,9 +66,100 @@ export default function Home() {
 
         {/* hero section */}
 
-        <div>v</div>
-        <div>c</div>
-        <div>s</div>
+        {/* short description */}
+
+        <div className="flex flex-col p-8 mt-9 mb-5">
+
+          <div className="flex flex-row items-center justify-between bg-linear-to-r from-[#2C60C2] to-[#32B34E] p-8 rounded-sm">
+
+            {/* description */}
+
+            <div className="flex flex-col max-w-lg text-justify leading-8">
+
+              <p className="text-[16px] font-semibold text-[#FFFFFF]">This Platform for Master in-demand skills with our comprehensive courses taught by industry experts. Start your learning journey today and transform your career. Master in-demand skills with our comprehensive courses taught by industry experts. Start your learning journey today and transform  your career.</p>
+
+            </div>
+
+            {/* description */}
+
+            {/* image */}
+
+            <div>
+
+              <Image src={'/assets/images/short descriptionimg.svg'} width={500} height={500} alt="short Image" className="rounded-sm"/>
+
+            </div>
+
+            {/* image */}
+            
+
+          </div>
+
+        </div>
+
+        <div className="flex flex-col border-t-2 border-b-2">
+
+          <div className="flex flex-row p-3 overflow-x-auto space-x-3">
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>
+
+            <div className="flex flex-col border justify-center items-center p-3 rounded-sm min-w-fit">
+
+              <span>Web Development</span>
+
+            </div>            
+
+          </div>
+
+        </div>
+
+        {/* short description */}
 
       </div>
 
