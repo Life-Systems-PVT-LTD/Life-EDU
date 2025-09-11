1db83ba912a6e0cf25fe39f88367ff961d8e21b1
Pasindu Nethsara <211049988+Life-Systems@users.noreply.github.com>
2025-09-11 20:08:30 +0530

    Completed like 10% fully responsive hero section Pasindu Nethsara 2025-09-11 8.08 Pm

--- life-edu/src/app/ourcourses/page.tsx
+++ life-edu/src/app/ourcourses/page.tsx
@@ -14,11 +14,35 @@ function OurCourses() {
 
             {/* header */}
 
+            {/* main content */}
+
             <div className='flex flex-col mt-[74px]'>
-                
+
                 {/* hero section */}
 
-                    <div className='flex flex-row bg-linear-to-r from-[#FB7185] to-[#F97316] h-[60vh]'></div>
+                <div className='flex flex-row bg-linear-to-r justify-center items-center from-[#FB7185] to-[#F97316] h-[60vh]'>
+
+                    {/* Description & header */}                    
+
+                    <div className='flex flex-col items-center gap-6 p-3'>
+
+                        <div className='flex flex-row tracking-widest'>
+
+                            <h1 className='text-[40px] font-bold text-[#020817]'>Our Courses</h1>
+
+                        </div>
+
+                        <div className='flex flex-row max-w-3xl text-center leading-10 tracking-widest'>
+
+                            <p className='text-[18px] font-semibold text-[#FFFFFF]'>Discover our very best, most interesting, valuable, and enjoyable courses carefully designed to inspire learning, spark creativity, and boost your skills. Each course is built with passion to make your journey exciting, meaningful, mastering new knowledge.</p>
+
+                        </div>
+
+                    </div>
+
+                    {/* Description & header */}
+
+                </div>
 
                 {/* hero section */}
 
@@ -28,6 +52,8 @@ function OurCourses() {
                 <div className='flex flex-row'></div>
 
             </div>
+
+            {/* main content */}
 
             {/* footer */}
 
