064e9e1250080b3de2f3a22f0e53aa9590935366
Pasindu Nethsara <211049988+Life-Systems@users.noreply.github.com>
2025-09-13 20:46:51 +0530

    search bard section create Pasindu Nethsara 2025-00-13 8.46 Pm

--- life-edu/src/app/ourcourses/page.tsx
+++ life-edu/src/app/ourcourses/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 import React from 'react'
+import { CiSearch } from 'react-icons/ci'
 
 function OurCourses() {
 
@@ -20,9 +21,9 @@ function OurCourses() {
 
                 {/* hero section */}
 
-                <div className='flex flex-row bg-linear-to-r justify-center items-center from-[#FB7185] to-[#F97316] h-[60vh]'>
+                <div className='flex flex-row bg-linear-to-r justify-center items-center from-[#FB7185] to-[#F97316] h-[60vh] mb-9'>
 
-                    {/* Description & header */}                    
+                    {/* Description & header */}
 
                     <div className='flex flex-col items-center gap-6 p-3'>
 
@@ -32,7 +33,7 @@ function OurCourses() {
 
                         </div>
 
-                        <div className='flex flex-row max-w-3xl text-center leading-10 tracking-widest'>
+                        <div className='flex flex-row max-w-3xl text-center leading-8 tracking-widest'>
 
                             <p className='text-[18px] font-semibold text-[#FFFFFF]'>Discover our very best, most interesting, valuable, and enjoyable courses carefully designed to inspire learning, spark creativity, and boost your skills. Each course is built with passion to make your journey exciting, meaningful, mastering new knowledge.</p>
 
@@ -46,7 +47,23 @@ function OurCourses() {
 
                 {/* hero section */}
 
-                <div className='flex flex-row'></div>
+                {/* search bar sesction */}
+
+                <div className='flex flex-row justify-end mt-9 p-6'>
+
+                    <div className='flex relative items-center'>
+
+                        <CiSearch className='absolute h-9 left-3 text-[#8793A6] font-bold'/>
+
+                        <input type="text" className='border-2 w-96 border-[#E9EEF4] h-11 pl-10 rounded-md' placeholder='Search course'/>
+
+                    </div>
+
+
+                </div>
+
+                {/* search bar sesction */}
+
                 <div className='flex flex-row'></div>
                 <div className='flex flex-row'></div>
                 <div className='flex flex-row'></div>
